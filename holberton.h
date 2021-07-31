4bf46646603f47fb15a3b18177961225176be0fa
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-31 18:55:16 +0000

    setup for project

---
+++ holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+#endif /*HOLBERTON_H*/
---
+++ printf.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _printf - a simple clone of the printf standard library function
+ * @format: format for the what is to be printed
+ *
+ * Return: number of bytes printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int bytes;
+	
+	return (bytes);
+}
