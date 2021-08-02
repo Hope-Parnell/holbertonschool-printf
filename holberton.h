a8402a5327c5bb956af98db5537a2f10d402ace3
Hope-Parnell <parnell.hope.c@gmail.com>
2021-08-02 18:09:33 +0000

    created basic outline for printf function and created a function to write and clear the buffer

--- holberton.h
+++ holberton.h
@@ -6,5 +6,6 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+void clearBuffer(char *buff, int *b, int *bc);
 
 #endif /*HOLBERTON_H*/
--- printf.c
+++ printf.c
@@ -9,7 +9,32 @@
 
 int _printf(const char *format, ...)
 {
-	int bytes;
+	int bytes, b = 0, f, bc = 0;
+	char *buffer;
+	va_list vars;
+
+	va_start(vars, format);
+	buffer = malloc(1024);
+	for (f = 0; format[f] != '\0'; f++)
+	{
+		if (b = 1024)
+			clearBuffer(buffer, &b, &bc); /*write and clear buffer if full*/
+		if (format[f] != '%')
+		{
+			buffer[b] = format[f];
+			b++;
+		}
+		else /*argument flag*/
+		{
+			f++;
+			/*call case fuction*/
+			va_arg(vars, int);
+		}
+	}
 	
+	write(1, buffer, b);
+	bytes = (1024 * bc) + b;
+	free(buffer);
+	va_end(vars);
 	return (bytes);
 }
