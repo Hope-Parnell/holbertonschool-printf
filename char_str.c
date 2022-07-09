18cd9eee381f2332fd2a99cd593bf2ada7d5a7ed
Hope-Parnell <parnell.hope.c@gmail.com>
2022-07-09 11:18:19 -0500

    rename header file

--- char_str.c
+++ char_str.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * f_char - handles characters for printf
--- printf.c
+++ printf.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * czech_flag - checks flags
  * @vars: variables
