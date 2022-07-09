80104cce6b719d6bd14b9467b372e7a94c2442c5
Hope-Parnell <parnell.hope.c@gmail.com>
2022-07-09 12:37:33 -0500

    updated INT_MIN and 0

--- int.c
+++ int.c
@@ -7,26 +7,29 @@
  */
 char *f_int(va_list vars)
 {
-	int i, j, is_neg = 0, n = va_arg(vars, int);
+	int i, j = 0, offset = 0, n = va_arg(vars, int);
 	char *tmp, t;
 
-	if (n == -2147483648)
-		return ("-2147483648");
-	if (n == 0)
-		return ("0");
 	tmp = malloc(12);
 	if (!tmp)
 		return (NULL);
+	if (n == 0)
+		tmp[offset++] = '0';
 	if (n < 0)
 	{
-		is_neg = 1;
-		tmp[0] = '-';
+		offset = 1;
+		tmp[j++] = '-';
 		n = -n;
+		if (n < 0)
+		{
+			tmp[j++] = '8';
+			n /= -10;
+		}
 	}
-	for (j = is_neg; n != 0; n /= 10, j++)
+	for (; n != 0; n /= 10, j++)
 		tmp[j] = (n % 10) + '0';
 	tmp[j] = '\0';
-	for (i = is_neg, j--; i < j; i++, j--)
+	for (i = offset, j--; i < j; i++, j--)
 	{
 		t = tmp[i];
 		tmp[i] = tmp[j];
